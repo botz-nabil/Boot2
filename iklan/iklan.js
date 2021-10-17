dfd4e8b2d45bd3f767764e1d53a30cc0b5558ebd
botz-nabil <nabilfirmansyah350@gmail.com>
2021-10-17 08:28:03 +0700

    Boot2

---
+++ iklan/iklan.js
@@ -0,0 +1,40 @@
+exports.iklan = (prefix) => {
+return `*LIST HARGA DIAMOND FREE FIRE VIA ID*
+*Proses 5-30 menit setelah admin cek bukti pembayaran*
+
+70 💎     : 10k
+
+100💎    : 15k
+
+140💎    : 20k
+
+ 210💎   : 30k
+ 
+ 495💎   : 68k
+ 
+ 720💎   : 98k
+ 
+ 1.440💎 : 195k
+ 
+ 2.000💎 : 261k
+ 
+ 3.440💎 : 460k
+ 
+ 4.000💎 : 522k
+ 
+ M.mingguan : 30k
+ M. Bulanan : 150k
+  
+
+Note : Pastikan ID Mu Benar Karena 
+Diamond Tidak Masuk Bukan 
+Tanggung Jawab Kami
+
+▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║
+▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║
+             T E R P E R C A Y A
+
+
+CHAT?? wa.me/6281938835391
+payment??? ketik #payment`
+}
---
+++ iklan/iklan2.js
@@ -0,0 +1,5 @@
+exports.iklan2 = (prefix) => {
+return `OPEN BUAT NOMER LUAR NEGERI
+MINAT CHAT?? wa.me/6281938835391
+payment???ketik #payment`
+}
